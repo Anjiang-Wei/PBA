25e12312af5ea5f44d5779728629561f2d1c119d
Anjiang-Wei <1020681930@qq.com>
2023-05-15 11:50:21 -0700

    try to create inter-chip latex table

---
+++ interchip_table/latex.py
@@ -0,0 +1,19 @@
+same = [
+    "../ember_capacity/",
+    "../ember_capacity2/",
+]
+other = [
+    "../intercapacity/",
+    "../intercapacity2/",
+]
+equal = [
+    "../bothcapacity/",
+    "../bothcapacity2/",
+]
+biased = [
+    "../domin_capacity/",
+    "../domin_capacity2/",
+]
+
+if __name__ == "__main__":
+    
